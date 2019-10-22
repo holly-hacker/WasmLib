bf5f58bfbb5652ac6d96b0b5459071fe409e4df7
HoLLy <dario.db@hotmail.com>
2019-10-22 22:18:10 +0200

    Create project

---
+++ WasmTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WasmTool
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
